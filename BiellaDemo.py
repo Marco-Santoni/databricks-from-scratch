df008766993a45cd355682c375a2d9f35c4de3d0
marcosantoni@hotmail.it <marcosantoni@hotmail.it>
2022-01-30 06:39:48 +0000

    1st round done

--- BiellaDemo.py
+++ BiellaDemo.py
@@ -17,7 +17,7 @@ dbutils.fs.ls('/databricks-datasets')
 # MAGIC %md
 # MAGIC ## IoT Dataset
 # MAGIC 
-# MAGIC List of IoT Devices with IP addresses. In addition to the IP address, the file contains the geographic information for each device entry:
+# MAGIC `/databricks-datasets/iot/iot_devices.json` is a list of IoT Devices with IP addresses. In addition to the IP address, the file contains the geographic information for each device entry:
 # MAGIC 
 # MAGIC - ISO-3166-1 two and three letter codes
 # MAGIC - Country Name
@@ -33,9 +33,9 @@ dbutils.fs.head("/databricks-datasets/iot/iot_devices.json")
 # MAGIC ## Interfaces
 # MAGIC 
 # MAGIC There are three key Spark interfaces that you should know about. [Ref](https://databricks.com/spark/getting-started-with-apache-spark/quick-start#spark-interfaces)
-# MAGIC - RDD
-# MAGIC - DataFrame (*most common*)
-# MAGIC - Datasets
+# MAGIC - RDD (*original data structure for Apache Spark*)
+# MAGIC - 💗 DataFrame (*most common today*)
+# MAGIC - Datasets (*Java and Scala only*)
 
 # COMMAND ----------
 
@@ -55,7 +55,9 @@ df
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Lazy evaluation: `df` is a definition but no data has been loaded yet.
+# MAGIC Lazy evaluation: `df` is a definition but **no data** has been loaded yet.
+# MAGIC 
+# MAGIC [Ref](https://stackoverflow.com/a/38028390/2314987) For transformations, Spark adds them to a DAG of computation and only when driver requests some data, does this DAG actually gets executed. One advantage of this is that Spark can make many **optimization** decisions after it had a chance to look at the DAG in entirety. This would not be possible if it executed everything as soon as it got it.
 
 # COMMAND ----------
 
@@ -82,6 +84,11 @@ display(
     (df.temp > 30) & (df.humidity > 70)
   )
 )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC We can add new transformations by pipelining commands on the dataframe
 
 # COMMAND ----------
 
@@ -132,6 +139,12 @@ df.createOrReplaceTempView("iot_devices")
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC Can I run queries over Spark SQL tables from outside?
+# MAGIC Yes, via [distributed SQL engine](https://spark.apache.org/docs/2.2.0/sql-programming-guide.html#distributed-sql-engine). Spark SQL can also act as a distributed query engine using its JDBC/ODBC or command-line interface. In this mode, end-users or applications can interact with Spark SQL directly to run SQL queries, without the need to write any code.
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ## Saving to CSV
 # MAGIC 
 # MAGIC What if we want to store the result to a csv file?
@@ -142,7 +155,7 @@ df.where(
   (df.temp > 30) & (df.humidity > 70)
 ).select('device_id', 'cn', 'humidity', 'temp')\
 .groupby('cn').avg('humidity', 'temp')\
-.write.csv('/tmp/test_aggregated_iot.csv')
+.write.mode('overwrite').csv('/tmp/test_aggregated_iot.csv')
 
 # COMMAND ----------
 
@@ -151,7 +164,7 @@ dbutils.fs.ls('/tmp/')
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Do you notice the trailing `/`?
+# MAGIC Do you notice the trailing `/`? `test_aggregated_iot.csv` is actually a folder.
 
 # COMMAND ----------
 
@@ -183,6 +196,8 @@ display(dbutils.fs.ls('/tmp/test_aggregated_iot_partitioned.csv/cn=Italy/'))
 
 # MAGIC %md
 # MAGIC How to read it again?
+# MAGIC 
+# MAGIC We can exploit the [partition discovery](https://spark.apache.org/docs/2.2.0/sql-programming-guide.html#partition-discovery) feature. Spark SQL will automatically extract the partitioning information from the paths
 
 # COMMAND ----------
 
@@ -195,8 +210,44 @@ display(df_2)
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC ## Reading from...
 # MAGIC 
-# MAGIC ## file parquet...
+# MAGIC Spark SQL supports reading data from a variety of formats including
+# MAGIC 
+# MAGIC - Parquet (default)
+# MAGIC - JSON
+# MAGIC - Hive tables
+# MAGIC - JDBC
+# MAGIC - Avro
+# MAGIC - csv
+# MAGIC - text
+# MAGIC 
+# MAGIC ## Parquet files
+# MAGIC 
+# MAGIC [Ref](https://databricks.com/glossary/what-is-parquet) Designed for efficient as well as performant flat **columnar** storage format of data compared to row based files like CSV or TSV files. Parquet is optimized to work with complex data in bulk and features different ways for efficient data **compression** and encoding types.
+# MAGIC 
+# MAGIC When querying, columnar storage you can **skip** over the non-relevant data very quickly. As a result, aggregation queries are less time consuming compared to row-oriented databases. Parquet can only read the needed columns therefore greatly minimizing the IO.
+
+# COMMAND ----------
+
+df.select('device_id', 'cn', 'humidity', 'temp')\
+  .write\
+  .mode('overwrite')\
+  .save('/tmp/test_aggregated_iot.parquet')
+
+# COMMAND ----------
+
+# MAGIC %fs ls /tmp/test_aggregated_iot.parquet/
+
+# COMMAND ----------
+
+# MAGIC %fs head /tmp/test_aggregated_iot.parquet/part-00000-d75d9dac-0f90-417a-aec8-529b4a6f4bf8-c000.snappy.parquet
+
+# COMMAND ----------
+
+display(
+  spark.read.load('/tmp/test_aggregated_iot.parquet')
+)
 
 # COMMAND ----------
 
@@ -205,7 +256,3 @@ display(df_2)
 # MAGIC ## Scheduling a Job
 # MAGIC 
 # MAGIC We'll now look at how to create a job
-
-# COMMAND ----------
-
-
